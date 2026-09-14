77dac8eb4c32b0625860688cc1aab515001d0e5a
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-09-13 22:00:08 -0500

    css pagina listo

--- src/pages/css.jsx
+++ src/pages/css.jsx
@@ -2,8 +2,26 @@ import "../styles/css.css";
 
 function CSS() {
   return (
-    <div>
-      <h1>CSS</h1>
+    <div className="css-container">
+      <h1 className="titulo">CSS</h1>
+      <p className="descripcion">
+        CSS, que significa Cascading Style Sheets (Hojas de Estilo en Cascada),
+        es un lenguaje utilizado para describir la presentación y el diseño de
+        un documento HTML. Mientras que HTML se encarga de estructurar el
+        contenido de una página web, CSS se encarga de definir cómo se verá ese
+        contenido, incluyendo aspectos como colores, fuentes, tamaños, márgenes
+        y disposición de los elementos.
+      </p>
+      <img src="../../public/css1.png" className="imagen" />
+      <p className="infoImagen">
+        Imagen 1: Código CSS aplicado a los elementos HTML de esta página web
+      </p>
+      <p className="descripcion">
+        CSS permite separar la estructura del contenido (HTML) de su
+        presentación, lo que facilita el mantenimiento y la actualización del
+        diseño de un sitio
+      </p>
+
     </div>
   );
 }
