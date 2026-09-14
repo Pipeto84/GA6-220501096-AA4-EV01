6e74777a55a00d77e779eb4e6c6dcb22f20a5289
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-09-13 22:06:56 -0500

    js pagina lista

--- src/pages/js.jsx
+++ src/pages/js.jsx
@@ -2,8 +2,24 @@ import "../styles/js.css";
 
 function JS() {
   return (
-    <div>
-      <h1>JavaScript</h1>
+    <div className="js-container">
+      <h1 className="titulo">JavaScript</h1>
+      <p className="descripcion">
+        JavaScript es un lenguaje de programación que se utiliza principalmente
+        para crear efectos interactivos en páginas web. Es un lenguaje de
+        programación de alto nivel, interpretado y orientado a objetos.
+      </p>
+      <img src="../../public/js1.png" className="imagen" />
+      <p className="infoImagen">
+        Imagen 1: Código JavaScript aplicado en la pregunta
+      </p>
+      <p className="descripcion">
+        JavaScript permite a los desarrolladores web crear aplicaciones web
+        dinámicas y mejorar la experiencia del usuario al permitir la
+        manipulación del contenido de la página sin necesidad de recargarla.
+        Además, JavaScript se utiliza en conjunto con HTML y CSS para crear
+        sitios web modernos y atractivos.
+      </p>
     </div>
   );
 }
