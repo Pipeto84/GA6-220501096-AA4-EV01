6fa29cfe51ef6f01f1418423bf41c71ac2c420e5
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-09-13 22:41:57 -0500

    pregunta js

--- src/pages/js.jsx
+++ src/pages/js.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import "../styles/js.css";
 
 function JS() {
+  const [resultado, setResultado] = useState(0);
+  const vistaResultado = () => {
+    if (resultado === 1) {
+      return <p className="respuesta-correcta">¡Respuesta correcta!</p>;
+    } else if (resultado === 2) {
+      return <p className="respuesta-incorrecta">Respuesta incorrecta</p>;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div className="js-container">
       <h1 className="titulo">JavaScript</h1>
@@ -20,6 +32,42 @@ function JS() {
         Además, JavaScript se utiliza en conjunto con HTML y CSS para crear
         sitios web modernos y atractivos.
       </p>
+      <div className="pregunta">
+        <h2 className="subtitulo">Pregunta:</h2>
+        <p className="pregunta-texto">
+          ¿Qué es JavaScript y para qué se utiliza en el desarrollo web?
+        </p>
+        <div className="respuesta">
+          <button
+            className="boton-respuesta"
+            onClick={() => setResultado(2)}
+          >
+            Estructurar el contenido visual y los textos de una página web
+            mediante etiquetas estáticas.
+          </button>
+          <button
+            className="boton-respuesta"
+            onClick={() => setResultado(2)}
+          >
+            Diseñar los colores, los estilos, las fuentes y la distribución
+            visual de los elementos en la pantalla.
+          </button>
+          <button className="boton-respuesta" onClick={() => setResultado(1)}>
+            Añadir interactividad, animaciones, validación de formularios y
+            contenido dinámico que responde al usuario.
+          </button>
+          <button
+            className="boton-respuesta"
+            onClick={() => setResultado(2)}
+          >
+            Gestionar y administrar de forma nativa los servidores físicos de
+            una empresa de hosting
+          </button>
+        </div>
+        <div className="resultado">
+          {vistaResultado()}
+        </div>
+      </div>
     </div>
   );
 }
